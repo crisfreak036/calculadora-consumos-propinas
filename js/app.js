5fbdcdc0bc28013a00ddbc04a0487486a94adf8c
crisfreak <cristopher.sotog@utem.cl>
2021-10-25 23:26:45 -0300

    Add function to request the menu to the API and other function to create a form for the menu

--- js/app.js
+++ js/app.js
@@ -6,6 +6,13 @@ let cliente = {
     pedido: []
 }
 
+// Objeto traductor de los id de categoría
+const categorias = {
+    1: 'Comida',
+    2: 'Bebidas',
+    3: 'Postres'
+}
+
 // Selector
 const btnGuardarCliente = document.querySelector('#guardar-cliente');
 const inputMesa = document.querySelector('#mesa');
@@ -47,6 +54,9 @@ function guardarCliente(){
 
     // Mostrar las secciones
     mostrarSecciones();
+
+    // Obtener platillos desde la API de JSON-SERVER
+    obtenerPlatillos();
 }
 
 function mostrarSecciones(){
@@ -54,5 +64,58 @@ function mostrarSecciones(){
     // Remueve la clase que oculta las secciones
     seccionesOcultas.forEach( seccion => {
         seccion.classList.remove('d-none');
+    });
+}
+
+function obtenerPlatillos(){
+    const url = 'http://localhost:4000/platillos';
+    fetch(url)
+        .then( respuesta => respuesta.json())
+        .then( platillos => mostrarPlatillos(platillos))
+        .catch( error => console.log(error))
+}
+
+// Crea el formulario para el ingreso de cantidades de platillos
+function mostrarPlatillos( platillos ){
+
+    // Selector del contenedor donde se mostrarán los platillos
+    const contenedorPlatillos = document.querySelector('#platillos .contenido');
+
+    platillos.forEach( platillo => {
+        const { categoria, id, nombre, precio } = platillo;
+
+        const row = document.createElement('DIV');
+        row.classList.add('row', 'py-3', 'border-top'); // Se le asigna la clase que da acceso al grid de bootstrap
+
+        // Crear contenedor para el nombre del platillo
+        const nombrePlatillo = document.createElement('DIV');
+        nombrePlatillo.classList.add('col-md-4');
+        nombrePlatillo.textContent = nombre;
+        
+        // Crear contenedor para el precio del platillo
+        const precioPlatillo = document.createElement('DIV');
+        precioPlatillo.classList.add('col-md-3', 'fw-bold');
+        precioPlatillo.textContent = `$${precio}`;
+        
+        // Crear contenedor para la categoría del platillo
+        const categoriaPlatillo = document.createElement('DIV');
+        categoriaPlatillo.classList.add('col-md-3');
+        categoriaPlatillo.textContent = categorias[categoria]; // Transforma de id a string la categoría
+
+        // Crear input para la cantidad de platillo a ordenar
+        const inputCantidad = document.createElement('INPUT');
+        inputCantidad.type = 'number';
+        inputCantidad.min = 0;
+        inputCantidad.value = 0;
+        inputCantidad.id = `producto-${id}`;
+        inputCantidad.classList.add('form-control'); // Clase que da estilo a los inputs en bootstrap
+        // Crea contenedor para el input con el objetivo de ingresar al grid de bootstrap
+        const contenedorInput = document.createElement('DIV');
+        contenedorInput.classList.add('col-md-2');
+        contenedorInput.appendChild(inputCantidad);
+
+        row.append(nombrePlatillo, precioPlatillo, categoriaPlatillo, contenedorInput);
+
+        contenedorPlatillos.appendChild(row);
     });
 }
